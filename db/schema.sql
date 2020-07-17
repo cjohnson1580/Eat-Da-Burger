2686d86124375304e17288c439f693d1bbd99c54
cjohnson1580 <cjohnson1580@gmail.com>
2020-07-17 12:08:09 -0500

    push to heroku

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,12 @@
-CREATE DATABASE burger_db;
+DROP DATABASE IF EXISTS burgers_db;
 
-USE burger_db;
+CREATE DATABASE burgers_db;
 
-CREATE TABLE BURGERS (
-  id          INT PRIMARY KEY AUTO_INCREMENT,
-  burger_name VARCHAR(255) NOT NULL,
-  devoured    BOOLEAN      NOT NULL
+USE burgers_db;
+
+CREATE TABLE burgers(
+  id INT NOT NULL AUTO_INCREMENT,
+  burger_name VARCHAR(50),
+  devoured BOOLEAN DEFAULT false,
+  PRIMARY KEY (id)
 );
